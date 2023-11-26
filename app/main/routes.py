fad9b222de2e95deaa5ea605ca7385f86c4ac804
Eseosa Atekha <eatekha@uwo.ca>
2023-11-26 07:31:35 -0500

    Connect Courses to thing Main Branch

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,29 @@
 from flask import render_template
 from flask_login import login_required
 from . import main
+import requests
+
+
+# Fetch this Route @app.route('/users/retrieveEnrolled', methods=['GET'])
+def getCourses():
+
+    # Define the API endpoint URL
+    api_url = 'http://127.0.0.1:3000/users/retrieveEnrolled'  # Replace with the actual API endpoint URL
+
+    try:
+        # Make a GET request to the API
+        response = requests.get(api_url)
+        print(response)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the response JSON data
+            data = response.json()
+            return data
+        else:
+            print(f"Failed to fetch data. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
 
 
 @main.route('/home')
@@ -11,4 +34,9 @@ def home():
             'title': '',
             'image': ''},
     ]
-    return render_template('home.html', cards=cards_data)
+
+        # Your dynamic data
+    data_array = getCourses()
+
+    # Pass the dynamic data as a variable to the template
+    return render_template('home.html', cards=cards_data, card_titles=data_array)
--- backend/main.py
+++ backend/main.py
@@ -131,7 +131,7 @@ def llm(course, school, topic):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
 
 
 
